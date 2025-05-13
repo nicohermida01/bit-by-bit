b213139f82e67d18cfe37fff7b74edb76e7465df
Nico Hermida <hermida.nicolas101@gmail.com>
2025-05-13 18:42:17 -0300

    add article menu icon

--- src/components/react/ArticleMenu.jsx
+++ src/components/react/ArticleMenu.jsx
@@ -6,6 +6,26 @@ import {
 	DropdownItem,
 } from '@heroui/react'
 
+const MenuIcon = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+		>
+			<path d='M4 12h16' />
+			<path d='M4 18h16' />
+			<path d='M4 6h16' />
+		</svg>
+	)
+}
+
 export function ArticleMenu({ items }) {
 	const handleItemClick = item => {
 		const elem = document.getElementById(item.slug)
@@ -15,7 +35,14 @@ export function ArticleMenu({ items }) {
 	return (
 		<Dropdown placement='bottom-end'>
 			<DropdownTrigger>
-				<Button variant='solid'>Menu</Button>
+				<Button
+					variant='solid'
+					isIconOnly
+					title='Menú de navegación'
+					aria-label='article navigation menu'
+				>
+					<MenuIcon />
+				</Button>
 			</DropdownTrigger>
 
 			<DropdownMenu aria-label='Article menu'>
